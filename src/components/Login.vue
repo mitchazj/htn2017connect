b2825bd8b0343baef2daf01831401cd45f0cac79
Mitchell Johnson <orangutandevelopment@gmail.com>
2017-09-15 05:26:19 -0400

    Playing with vue-router

---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <h1>{{ msg }}</h1>
+    <h2>YER CHECK THIS OUT {{ memes }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'DIS IS A NEW PAGE',
+      memes: "<yee boi>"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
